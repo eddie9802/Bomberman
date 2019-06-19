962e28771e13bb6cf43e44fc5640d2b24d054a67
Matthew Rainey <rainey-m7@ulster.ac.uk>
2019-06-19 21:24:04 +0100

    Added images and sprite animation logic

--- bomberman.py
+++ bomberman.py
@@ -1,9 +1,27 @@
-class bomberman:
-    x_pos = 0
-    y_pos = 0
+import pygame
+
+class bomberman():
 
     def __init__(self, x_pos, y_pos):
+        self.image = pygame.image.load("SBred.png")
         self.x_pos = x_pos
         self.y_pos = y_pos
+
+    def handle_keys(self):
+        """ Handles Keys """
+        key = pygame.key.get_pressed()
+        DIST = 5 # distance moved in 1 frame, try changing it to 5
+        if key[pygame.K_DOWN]: # down key
+            self.y_pos += DIST # move down
+        elif key[pygame.K_UP]: # up key
+            self.y_pos -= DIST # move up
+        if key[pygame.K_RIGHT]: # right key
+            self.x_pos += DIST # move right
+        elif key[pygame.K_LEFT]: # left key
+            self.x_pos -= DIST # move left
+
+    def draw(self, surface):
+        # blit bomberman at the current position
+        surface.blit(self.image, (self.x_pos, self.y_pos))
 
     # def setup_graphics():
--- entry.py
+++ entry.py
@@ -3,8 +3,6 @@ from bomberman import bomberman
 from wall import wall
 from maze import maze
 
-# pygame.draw.rect(screen, (0, 200, 0), pygame.Rect(30, 30, 60, 60))
-
 BLACK = (0, 0, 0)
 
 MARGIN = 5
@@ -12,29 +10,37 @@ MARGIN = 5
 def main():
     
     # Game setup
-    b = bomberman(0, 0)
-    m = maze(b)
-    m.build()
+    Bomberman = bomberman(0, 0)
+    Maze = maze(Bomberman)
+    Maze.build()
 
     # Graphics setup
     pygame.init()
     pygame.display.set_caption('Bomberman')
+
+    # Start screen logic
+    screen = pygame.image.load("startscreen.png")
+
+    pygame.Start()
+
     screen = pygame.display.set_mode([1200, 800])
-    bg = pygame.transform.scale(pygame.image.load("bg.png"), (1200, 800))
-    screen.blit(bg, (0, 0))
+    clock = pygame.time.Clock()
 
     playing = True
 
     while playing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 playing = False
-            pressed = pygame.key.get_pressed()
-            """ if pressed[pygame.K_UP]:
-                if pressed[pygame.K_DOWN]: 
-                if pressed[pygame.K_LEFT]: 
-                if pressed[pygame.K_RIGHT]: """
-        pygame.display.flip()
+                
+        Bomberman.handle_keys() # handle the keys
+
+        screen.fill((255,255,255)) # fill the screen with white
+        Bomberman.draw(screen) # draw the bird to the screen
+        pygame.display.update() # update the screen
+
+        clock.tick(40)
         
 if __name__ == "__main__":
     main()
